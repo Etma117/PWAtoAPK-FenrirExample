dcdafaba7287b810d951a8e44a7dd2ff82d0da64
エスマ117 <polllfuentes@gmail.com>
2024-05-14 11:21:23 -0600

    WebView - WebClient

--- app/src/main/java/com/example/fenrirapk/MainActivity.java
+++ app/src/main/java/com/example/fenrirapk/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.fenrirapk;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebSettings; // Import for enabling JavaScript
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +22,23 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setJavaScriptEnabled(true); // Uncomment if necessary
 
         webView.loadUrl("https://fenrir-master-v2.onrender.com/");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // Check if the URL belongs to your PWA domain
+                if (url.startsWith("https://fenrir-master-v2.onrender.com/")) {
+                    // Load the URL within the WebView
+                    view.loadUrl(url);
+                    return true; // Return true to prevent opening in the browser
+                } else {
+                    // Open the URL in the external browser
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(intent);
+                    return false; // Return false to allow opening in the browser
+                }
+            }
+        });
+        webView.loadUrl("https://fenrir-master-v2.onrender.com/");
     }
 }
+
